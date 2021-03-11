0568de9ce354c6ac3a7f4641b71ba61084a2cea1
Natalia Cieplińska <cieplins@agh.edu.pl>
2021-03-11 21:42:21 +0100

    Fixed title bug for IEEE, added publication name (journal) column, changed column names, deleted rownumbers

--- lit_rev_tool.R
+++ lit_rev_tool.R
@@ -4,12 +4,14 @@ library(plyr)
 library(tidyverse)
 library(dplyr)
 library(bibliometrix)
+
+
 my_dir <- choose.dir(default = "", caption = "Select folder")
 setwd(my_dir)
 #setwd(my_path_dir)
 iee_fix <- function(iee_file){
   tx  <- readLines(iee_file)
-  tx2  <- gsub(pattern = "title", replace = "\ntitle", x = tx)
+  tx2  <- gsub(pattern = "}, title", replace = "}, \ntitle", x = tx)
   writeLines(tx2, con=iee_file)
 }
 
@@ -23,7 +25,7 @@ lapply(iee_files, function(x){
 conversion <- function(some_file) {
   my_set <- convert2df(some_file, dbsource="wos", format="bibtex")
   my_set$file_source <- some_file
-  keeps <- c("TI", "AB", "AU", "DI", "PY", "file_source")
+  keeps <- c("TI", "AB", "AU", "DI", "PY", "SO", "file_source")
   dtbs = my_set[keeps]
   return(dtbs)
 }
@@ -31,14 +33,22 @@ conversion <- function(some_file) {
 file_bib <- list.files(my_dir, pattern = "*.bib")
 final_dtbs = lapply(file_bib, function(x){
   conversion(x)
-
+  
 })
 
 df = do.call("rbind", final_dtbs)
 df$relatable <- NA
 df %>% group_by(TI) %>% mutate(sources = paste0(file_source, collapse="/")
-                               )-> out
-keeps <- c("TI", "AB", "AU", "DI", "PY", "sources", "relatable")
+)-> out
+keeps <- c("TI", "AB", "AU", "DI", "PY", "SO", "sources", "relatable")
 out = out[keeps]
-out_data <- out[!duplicated(out$TI), ]
-write.csv(out_data, "merged.csv")
+names(out)[names(out) == "TI"] <- "Title"
+names(out)[names(out) == "AB"] <- "Abstract"
+names(out)[names(out) == "AU"] <- "Authors"
+names(out)[names(out) == "DI"] <- "DOI"
+names(out)[names(out) == "PY"] <- "Year"
+names(out)[names(out) == "SO"] <- "Publication name"
+out_data <- out[!duplicated(out$Title), ]
+
+trial <- convert2df("acm.bib", dbsource="wos", format="bibtex")
+write.csv(out_data, "merged.csv", row.names=FALSE)
